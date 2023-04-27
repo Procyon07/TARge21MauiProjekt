b0cfa817d2a906f52b31d0af651d59c882ced14d
Opilane TTHK <opilane@tthk.ee>
2023-04-27 15:42:10 +0300

    CoffeeModel

---
+++ TARge21MauiProjekt/GlobalUsings.cs
@@ -0,0 +1,2 @@
+﻿global using Views;
+global using Services;
---
+++ TARge21MauiProjekt/Models/Coffee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Coffee
+    {
+        public string Name { get; set; }
+        public string Subtitle { get; set; }
+        public string HeroImage { get; set; }
+        public string Description { get; set; }
+        public Color AccentColorStart { get; set; }
+        public Color AccentColorEnd { get; set; }
+        public List<string> Images { get; set; }
+       
+    }
+}
