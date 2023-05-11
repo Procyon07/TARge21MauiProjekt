becce8912d0dad79107a222cd1238a15118fe4db
Opilane TTHK <opilane@tthk.ee>
2023-05-11 13:06:26 +0300

    firstCoffee

--- TARge21MauiProjekt/Services/CoffeeServices.cs
+++ TARge21MauiProjekt/Services/CoffeeServices.cs
@@ -6,5 +6,24 @@ namespace Services
 {
     internal class CoffeeServices
     {
+        private static List<Coffee> coffees = new()
+        {
+            new()
+            {
+                Name = "Cappucino",
+                Subtitle = "The deliciously indulgent cup.",
+                Description = "The perfect blend of creamy froth, bold espresso, and a sprinkle of cocoa magic, all in one deliciously indulgent cup",
+                HeroImage = "cappucino.png",
+                AccentColorStart = Color.FromArgb("#0c293d"),
+                AccentColorEnd = Color.FromArgb("#26abe0"),
+                Images = new ()
+                {
+                    "https://www.acouplecooks.com/wp-content/uploads/2020/10/how-to-make-cappuccino-005.jpg",
+                    "https://kawazmleczkiem.pl/app/uploads/2020/12/CAPPUCINO-1024x682-min-1024x682.jpg",
+                    "https://noir.web.id/wp-content/uploads/2022/04/jual-cappucino-terdekat.jpg"
+
+                }
+            }
+        };
     }
 }
