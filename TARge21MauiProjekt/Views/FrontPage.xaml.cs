3b364895be2f2d5b4895906cd40a9ca3023a3a02
Opilane TTHK <opilane@tthk.ee>
2023-05-11 15:23:44 +0300

    BasicFrontPage
    Also added styling

--- TARge21MauiProjekt/Views/FrontPage.xaml.cs
+++ TARge21MauiProjekt/Views/FrontPage.xaml.cs
@@ -6,4 +6,23 @@ public partial class FrontPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        if (this.AnimationIsRunning("TransistionAnimation"))
+        {
+            return;
+        }
+        var parentAnimation = new Animation();
+
+        //parentAnimation.Add(0, 0.2, new Animation(v => imgMercury.Opacity = v, 0, 1, Easing.CubicIn));
+        
+        parentAnimation.Commit(this, "TransitionAnimation", 16, 3000, null, null);
+
+        //Intro Box
+        parentAnimation.Add(0.7, 1, new Animation(v => frmIntro.Opacity = v, 0, 1, Easing.CubicIn));
+
+    }
+    async void Welcome_Clicked(System.Object sender, System.EventArgs e)
+        => Application.Current.MainPage = new NavigationPage(new CoffeesPage());
 }
