6539095b49e800b578e23c6b2e9c5a0372f6dc00
Opilane TTHK <opilane@tthk.ee>
2023-05-11 14:29:02 +0300

    Update CoffeeDetailsPage.xaml.cs

--- TARge21MauiProjekt/Views/CoffeeDetailsPage.xaml.cs
+++ TARge21MauiProjekt/Views/CoffeeDetailsPage.xaml.cs
@@ -2,8 +2,14 @@ namespace Views;
 
 public partial class CoffeeDetailsPage : ContentPage
 {
-	public CoffeeDetailsPage()
+	public CoffeeDetailsPage(Coffee coffee)
 	{
 		InitializeComponent();
+
+		this.BindingContext = coffee;
 	}
+    async void BackButton_Clicked(System.Object sender, System.EventArgs e)
+    {
+        await Navigation.PopAsync();
+    }
 }
